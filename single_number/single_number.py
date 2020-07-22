693581c32debe46c4954afff709795eb0bddd51e
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-07-22 16:14:06 -0700

    single number passing

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,14 +1,23 @@
-'''
+"""
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
-'''
+"""
+
+
 def single_number(arr):
-    # Your code here
+    import collections
 
-    pass
+    counter = collections.defaultdict(int)
+
+    for elem in arr:
+        counter[elem] += 1
+
+    for key, val in counter.items():
+        if val < 2:
+            return key
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
