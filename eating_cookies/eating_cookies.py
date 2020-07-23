54e1a873b7b2098777029574a91ce1e00dedac56
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-07-23 11:18:47 -0700

    cache improved for eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -17,27 +17,44 @@ Returns: an integer
 
 
 # attempt w/ cache
-def eating_cookies(n, cache=[]):
+import collections
+
+
+def eating_cookies(n, cache=collections.defaultdict(int)):
     if n < 0:
         return 0
-    elif n < 2:
+    if n == 0:
         return 1
-    elif cache[n] > 0:
+    if cache[n]:
         return cache[n]
 
     else:
         cache[n] = (
-            eating_cookies(n - 3, cache)
+            eating_cookies(n - 1, cache)
             + eating_cookies(n - 2, cache)
-            + eating_cookies(n - 1, cache)
+            + eating_cookies(n - 3, cache)
         )
-    return cache[n]
+        return cache[n]
+
+
+# iterative
+# def eating_cookies(n, cache=None):
+#     answer = 0
+#     c1 = 1
+#     c2 = 2
+#     c3 = 3
+#     for i in range(n - 1):
+#         answer = c1 + c2 + c3
+#         c3 = c2
+#         c2 = c1
+#         c1 = answer
+#     return answer
 
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
-
+    cache = {}
     print(
-        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies"
+        f"There are {eating_cookies(num_cookies, cache)} ways for Cookie Monster to each {num_cookies, cache} cookies"
     )
